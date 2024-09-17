a3c9f03f3b5a71a637b67d275a990919798e8ee0
diegosarasaGO <diego.sarasa@grupocean.com>
2024-09-17 20:16:43 +0200

    init

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,3 @@
-// Parte 1: Listar el nombre de todos los ficheros ubicados en un directorio (en clase hemos puesto como ejemplo un directorio creado en el paquete resources)
-//
-// Parte 2: Crear un subdirectorio dentro de la carpeta anterior y listar el nombre de todos los archivos
-//
-// Parte 3: Listar el nombre de todos los ficheros y directorios de una ruta concreta, como por ejemplo C:/Users
-//
-// Para la parte 3 tenéis que utilizar el concepto de recursividad. Investigad a que se refiere ese término y en la clase del jueves lo trataremos, así como la resolución del ejercicio.
-
 import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
